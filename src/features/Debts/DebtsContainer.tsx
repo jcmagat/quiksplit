5f41719b71902990e4cb66e44d2331a5f32785a7
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-07-11 10:37:59 -0700

    Improve typing in DebtsContainer

--- src/features/Debts/DebtsContainer.tsx
+++ src/features/Debts/DebtsContainer.tsx
@@ -1,16 +1,17 @@
 import { useSelector } from "react-redux";
 import { RootState } from "../../redux/store";
+import { Friend } from "../../types";
 
 interface Debt {
   debtorId: number; // person who owes the debt
-  debteeId: number; // person to whom the debt is owed
-  debt: number;
+  creditorId: number; // person to whom the debt is owed
+  amount: number;
 }
 
 function DebtsContainer() {
   const friends = useSelector((state: RootState) => state.friends);
 
-  const friendsEntries = Object.entries(friends);
+  const friendsEntries = Object.entries(friends) as [string, Friend][];
   const count = Object.keys(friends).length;
 
   let total = 0;
@@ -34,40 +35,46 @@ function DebtsContainer() {
       if (difference > 0) {
         debts.push({
           debtorId: parseInt(nextId),
-          debteeId: parseInt(id),
-          debt: difference,
+          creditorId: parseInt(id),
+          amount: difference,
         });
       } else if (difference < 0) {
         debts.push({
           debtorId: parseInt(id),
-          debteeId: parseInt(nextId),
-          debt: Math.abs(difference),
+          creditorId: parseInt(nextId),
+          amount: Math.abs(difference),
         });
       }
     }
   }
 
   return (
-    <div className="debts-container">
-      <h4 className="debts-title">
-        Total expenses:
-        <span>{`$${total.toFixed(2)}`}</span>
-      </h4>
+    <>
+      {count > 0 && (
+        <div className="debts-container">
+          <h4 className="debts-title">
+            Total expenses:
+            <span>{`$${total.toFixed(2)}`}</span>
+          </h4>
 
-      <p className="debts-subtitle">
-        Each person pays
-        <span>{`$${(total / count).toFixed(2)}`}</span>
-      </p>
+          <p className="debts-subtitle">
+            Each person pays
+            <span>{`$${(total / count).toFixed(2)}`}</span>
+          </p>
 
-      <ul className="debts-list">
-        {debts.map((debt, index) => (
-          <li key={index}>
-            {`${friends[debt.debtorId].name} ➡ ${friends[debt.debteeId].name}`}
-            <span>{`$${debt.debt.toFixed(2)}`}</span>
-          </li>
-        ))}
-      </ul>
-    </div>
+          <ul className="debts-list">
+            {debts.map((debt, index) => (
+              <li key={index}>
+                {`${friends[debt.debtorId].name} ➡ ${
+                  friends[debt.creditorId].name
+                }`}
+                <span>{`$${debt.amount.toFixed(2)}`}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </>
   );
 }
 
