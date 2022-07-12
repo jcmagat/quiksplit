2ecdb1f9823885294ab70e4691bc9f009f1a6af4
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-07-12 12:07:48 -0700

    Change app name to QuikSplit

--- src/features/Navigation/index.tsx
+++ src/features/Navigation/index.tsx
@@ -2,7 +2,7 @@ function Navigation() {
   return (
     <div className="nav">
       <h1 className="nav-title">
-        Quick
+        Quik
         <span>Split</span>
       </h1>
       <p className="nav-subtitle">Quickly split expenses with your friends</p>
