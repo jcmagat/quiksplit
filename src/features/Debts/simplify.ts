d07ec9d78c381cc2a1672cdd0927a04ff98e50c6
Juan Carlo Magat <magatjuancarlo@gmail.com>
2022-07-21 15:45:31 -0700

    Add typescript to simplify()

--- src/features/Debts/simplify.ts
+++ src/features/Debts/simplify.ts
@@ -1,30 +1,19 @@
-function getMin(arr) {
+function getMin(arr: number[]) {
   return arr.indexOf(Math.min(...arr));
 }
 
-function getMax(arr) {
+function getMax(arr: number[]) {
   return arr.indexOf(Math.max(...arr));
 }
 
-// Given a set of persons as graph
-// where graph[i][j] indicates
-// the amount that person i needs to
-// pay person j, this function
-// finds and prints the minimum
-// cash flow to settle all debts.
-function simplify(graph) {
+function simplify(graph: number[][]) {
   // TODO: validate graph so that it doesn't produce an infinite loop
-  // 1. check for NaN !!important
-  // 2. check for negative numbers
+  // - check for NaN !!important
+  // - check if matrix is a square (length = width) !!important
+  // - check for negative numbers
 
-  let amount = Array(graph.length).fill(0);
+  let amount = Array<number>(graph.length).fill(0);
 
-  // Calculate the net amount to
-  // be paid to person 'p', and
-  // stores it in amount[p]. The
-  // value of amount[p] can be
-  // calculated by subtracting
-  // debts of 'p' from credits of 'p'
   for (let p = 0; p < amount.length; p++) {
     for (let i = 0; i < amount.length; i++) {
       amount[p] += graph[i][p] - graph[p][i];
@@ -35,7 +24,7 @@ function simplify(graph) {
 
   // Finish the while loop when every number in amount is 0
   // (i.e. when all debts are settled)
-  while (!amount.every((num) => Number(num.toFixed(2)) === 0)) {
+  while (amount.some((num) => Number(num.toFixed(2)) !== 0)) {
     // console.log("before: ", amount);
     // console.log("creditor: ", creditorId, " | debtor: ", debtorId);
     // console.log(
